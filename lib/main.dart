26271459f02f0bc3f03c856456f2cc4c2100eed2
Johannes Hehl <st4ndd@outlook.com>
2026-06-11 11:56:23 +0200

    Initial Flutter test app with start screen and iOS build workflow

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First Flutter App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const StartScreen(),
+    );
+  }
+}
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Flutter App'),
+      ),
+      body: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
